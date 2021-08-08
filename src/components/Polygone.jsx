9ab2654d4e53434627a3599ed357ceb208515727
Evgenii <13196402+kmm7@users.noreply.github.com>
2021-08-08 12:33:58 +0200

    Polygon added in have fun

--- src/components/Polygone.jsx
+++ src/components/Polygone.jsx
@@ -1,15 +1,23 @@
+import React, {useState} from "react";
 import { Slider, Typography } from "@material-ui/core";
+import PolygoneDrawer from "./PolygoneDrawer";
 
 export default function Polygone(){
+    const [corners, setCorners] = useState(3);
+
     return (
         <div>
+            <PolygoneDrawer nCorners={corners}></PolygoneDrawer>
             <Typography>
                 # corners
             </Typography>
             <Slider
+                value={corners}
                 aria-labelledby="discrete-slider"
                 valueLabelDisplay="auto"
                 marks
+                onChange = {(e,v) => setCorners(v)}
+                controlled = "true"
                 min={3}
                 max={10}
             />
